56c93b271a3d8c33a713c6e024a635e41c198efc
Jinny Yan <jinnyyan8@gmail.com>
2019-02-19 09:46:42 -0500

    Currently not working button or receiver

---
+++ bridge/master-receiver/master-receiver.ino
@@ -0,0 +1,65 @@
+#include <ELECHOUSE_CC1101.h>
+#include <Servo.h>
+
+Servo myservo; // create servo object to control a servo
+
+int pos = 0; // variable to store the servo position
+byte moving = '0'; // variable to store whether servo is moving
+byte opened = '0'; // variable to store whether bridge is open (this reflects master)
+const int n = 61; // size of buffer
+byte  buffer[2] = {0};
+
+void setup(){
+  Serial.begin(9600); // begins for writing to serial monitor
+  Serial.println("Responder Initating");
+
+  ELECHOUSE_cc1101.Init(F_868);  // set frequency - F_433, F_868, F_965 MHz
+  ELECHOUSE_cc1101.SetReceive();
+  myservo.attach(9); // attaches the servo on pin 9 of the servo object
+  initiatePosition(); // upon initiating, bridge should be closed at the 0 position
+  }
+
+void loop()
+{
+  if (ELECHOUSE_cc1101.CheckReceiveFlag())
+  {
+    int len = ELECHOUSE_cc1101.ReceiveData(buffer);
+    moving = buffer[0];
+    opened = buffer[1];
+    Serial.println(moving);
+    if (moving == '1'){ // TODO: Check value of what moving char is
+      movePosition();
+      ELECHOUSE_cc1101.SetReceive();
+    }
+  }
+}
+
+void movePosition() {
+  if (pos == 0 && opened == '1') {         // Bridge is closed, heading towards open
+    while (pos < 90) {
+      pos++;
+      myservo.write(pos);
+      delay(15);
+    }
+  } else if (pos == 90 && opened == '0') { // Bridge is open, heading towards closed
+    while (pos > 0) {
+      pos--;
+      myservo.write(pos);
+      delay(15);
+    }
+  }
+}
+
+void initiatePosition() {
+  // Ensure servo is at position 0 at the beginning
+  while (pos < 0) {
+    pos++;
+    myservo.write(pos);
+    delay(15);
+  }
+  while (pos > 0) {
+    pos--;
+    myservo.write(pos);
+    delay(15);
+  }
+}
---
+++ bridge/master-sender/master-sender.ino
@@ -0,0 +1,63 @@
+#include <ELECHOUSE_CC1101.h>
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int pos = 0;    // variable to store the servo position
+byte moving = '0'; // variable to store whether servo is moving
+byte opened = '0'; // variable to store whether bridge is open
+const int n = 61; // size of buffer
+
+void setup() {
+  Serial.begin(9600); // begins for writing to serial monitor
+  Serial.println("Transponder initiating");
+  
+  ELECHOUSE_cc1101.Init(F_868); // set frequency - F_433, F_868, F_965 MHz
+  pinMode(4, INPUT);  // attaches the button on pin 4
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  initiatePosition(); // upon initiating, bridge should be closed at the 0 position
+}
+
+void loop() {
+  int status = digitalRead(4); // status of button
+  if (status == 0) { // when button is pressed down
+    moving = '1';
+    movePosition();
+    ELECHOUSE_cc1101.SendData(moving, '1');
+    ELECHOUSE_cc1101.SendData(opened, '1');
+  } else if (status == 1) { // when button is released
+    moving = false;
+  }
+}
+
+void movePosition() {
+  if (pos == 0){          // Bridge is closed, heading towards open
+    while(pos < 90){
+      pos++;
+      myservo.write(pos);
+      delay(15);
+    }
+    opened = '1';
+  } else if (pos == 90) { // Bridge is open, heading towards closed
+     while (pos > 0){
+      pos--;
+      myservo.write(pos);
+      delay(15);
+    }
+    opened = '0';
+  } 
+}
+
+void initiatePosition() {
+  // Ensure servo is at position 0 at the beginning
+  while(pos < 0){
+      pos++;
+      myservo.write(pos);
+      delay(15);
+  }
+  while (pos > 0){
+    pos--;
+    myservo.write(pos);
+    delay(15);
+  }
+}
